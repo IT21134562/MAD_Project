ff516e9eec41158c43ffbcbf515807900e4ccebb
Menusha <it21113772@my.sliit.lk>
2022-10-29 23:04:57 +0530

    Function add workout to do

---
+++ app/src/main/java/net/smallacademy/authenticatorapp/OnDialogCloseListner.java
@@ -0,0 +1,8 @@
+package net.smallacademy.authenticatorapp;
+
+import android.content.DialogInterface;
+
+public interface OnDialogCloseListner {
+
+    void onDialogClose(DialogInterface dialogInterface);
+}
